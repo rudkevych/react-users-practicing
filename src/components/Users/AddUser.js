7d25bbf8be819a3e8bc8d427758702e509340c4b
rudkevych <oksana.rudkevych@gmail.com>
2022-09-07 23:36:45 +0200

    style: added styles for user form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,22 @@
-import Card from '../UI/Card';
+import Card from "../UI/Card";
+import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
     console.log(event);
-  }
+  };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <Card></Card>
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
