6175c080b4484555926ade52e93c82dc62da9795
rudkevych <oksana.rudkevych@gmail.com>
2022-09-08 00:12:47 +0200

    feat: added main functionality to Button component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
-import Card from "../UI/Card";
+import Card from '../UI/Card';
 import styles from './AddUser.module.css';
+import Button from '../UI/Button';
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -8,13 +9,13 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card className={styles.input}>
+    <Card className={styles.input} >
       <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' />
+        <label htmlFor='age'>Age</label>
+        <input id='age' type='number' />
+        <Button type='submit'>Add User</Button>
       </form>
     </Card>
   );
