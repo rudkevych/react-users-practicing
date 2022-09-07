4a3f49e1cada4fb93fa33ff130d4967f0968f742
rudkevych <oksana.rudkevych@gmail.com>
2022-09-08 00:22:39 +0200

    feat: form resetting, two-way binding

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,33 @@
 import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import Button from '../UI/Button';
+import { useState } from 'react';
 
 const AddUser = (props) => {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(event);
+    console.log(username, age);
+    setUsername('');
+    setAge('');
+  };
+
+  const usernameChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
   };
 
   return (
-    <Card className={styles.input} >
+    <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input id='username' type='text' value={username} onChange={usernameChangeHandler} />
         <label htmlFor='age'>Age</label>
-        <input id='age' type='number' />
+        <input id='age' type='number' value={age} onChange={ageChangeHandler} />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
