fa8706c50ef685d47b16be55dce0bfc829cd1b49
rudkevych <oksana.rudkevych@gmail.com>
2022-09-10 00:09:49 +0200

    fix: moved modal to root using portal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,41 @@
 import Card from './Card';
 import Button from './Button';
 import styles from './ErrorModal.module.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onClose} />;
+};
+
+const Modal = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onClose}>Ok</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = (props) => {
-  //   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onClose} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onClose}>Ok</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose}/>,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <Modal title={props.title} message={props.message} onClose={props.onClose}/>,
+        document.getElementById('modal-root')
+      )}
+    </>
   );
 };
 
